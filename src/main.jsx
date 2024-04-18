49d14ddfd572ae5c34ce24e37e6289fb442a47cf
Davide Spada <dav.spada@outlook.com>
2024-04-18 13:20:02 +0200

    add redirect

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import './App.css'
+//import './index.css'
 import {
   createBrowserRouter,
   RouterProvider,
+  Navigate
 } from "react-router-dom";
 import SignInSide from './components/SignInSide.jsx';
 import SignUp from './components/SignUp.jsx';
-import Orders from './components/Orders.jsx';
-import Dashboard from './components/Dashboard.jsx';
-import Dashboard2 from './components/Dashboard2.jsx';
-import Dashboard3 from './components/Dashboard3.jsx';
 import Dashboard4 from './components/Dashboard4.jsx'
 
 const router = createBrowserRouter([
@@ -26,6 +23,10 @@ const router = createBrowserRouter([
   {
     path: "/dashboard",
     element: <Dashboard4 />
+  },
+  {
+    path: "*",
+    element: <Navigate to="/login" />
   }
 ])
 ReactDOM.createRoot(document.getElementById('root')).render(
